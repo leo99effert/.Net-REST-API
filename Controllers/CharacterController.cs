9dbc981ac3a4c35162ea291dddf7df958a5e0c09
leo99effert <leo99effert@outlook.com>
2023-03-28 13:43:53 +0200

    character service

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -6,25 +6,29 @@ namespace Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static List<Character> characters = new List<Character> {
-            new Character(),
-            new Character{ Id = 1, Name = "Sam" }
-        };
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+        _characterService = characterService;
+        }
 
         [HttpGet("GetAll")]
-        public ActionResult<List<Character>> Get() {
-            return Ok(characters);
+        public ActionResult<List<Character>> Get() 
+        {
+            return Ok(_characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public ActionResult<List<Character>> GetSingle(int id) {
-            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        public ActionResult<List<Character>> GetSingle(int id) 
+        {
+            return Ok(_characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public ActionResult<List<Character>> AddCharacter(Character newCharacter) {
-            characters.Add(newCharacter);
-            return Ok(characters);
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter) 
+        {
+            return Ok(_characterService.AddCharacter(newCharacter));
         }
     }
 }
