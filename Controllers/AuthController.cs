f7287a60766bae7115cc7bbe0dbfa0612a70b1f3
leo99effert <leo99effert@outlook.com>
2023-03-29 20:42:35 +0200

    login

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+global using Microsoft.AspNetCore.Mvc;
+global using Dtos.User;
+
+namespace Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class AuthController : ControllerBase
+  {
+    private readonly IAuthRepository _authrepo;
+    public AuthController(IAuthRepository authrepo)
+    {
+      _authrepo = authrepo;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+    {
+        var response = await _authrepo.Register(
+            new User { Username = request.Username }, request.Password
+        );
+        if(!response.Success)
+        {
+            return BadRequest(response);
+        }
+        return Ok(response);
+    }
+
+    [HttpPost("Login")]
+    public async Task<ActionResult<ServiceResponse<int>>> Login(UserLoginDto request)
+    {
+        var response = await _authrepo.Login(request.Username, request.Password);
+        if(!response.Success)
+        {
+            return BadRequest(response);
+        }
+        return Ok(response);
+    }
+  }
+}
