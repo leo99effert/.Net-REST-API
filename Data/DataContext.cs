12a9ce595e94c07685b8994628d0c8e290ab2f38
leo99effert <leo99effert@outlook.com>
2023-03-29 19:48:53 +0200

    database

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Character> Characters => Set<Character>();
+        public DbSet<User> Users => Set<User>();
+    }
+}
