d29288e57e4a7842883843cc928896cac88ad45d
leo99effert <leo99effert@outlook.com>
2023-03-31 15:04:12 +0200

    small syntax fix

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -13,30 +13,21 @@ namespace Controllers
     [HttpPost("register")]
     public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
     {
-      var response = await _authrepo.Register(
-          new User { Username = request.Username }, request.Password
-      );
-      if (!response.Success)
-      {
-        return BadRequest(response);
-      }
+      var response = await _authrepo.Register(new User { Username = request.Username }, request.Password);
+      if (!response.Success) return BadRequest(response);
       return Ok(response);
     }
     [HttpPost("Login")]
     public async Task<ActionResult<ServiceResponse<int>>> Login(UserLoginDto request)
     {
       var response = await _authrepo.Login(request.Username, request.Password);
-      if (!response.Success)
-      {
-        return BadRequest(response);
-      }
+      if (!response.Success) return BadRequest(response);
       return Ok(response);
     }
     [HttpDelete]
     public async Task<ActionResult<ServiceResponse<int>>> Delete(int userId)
     {
-      var response = await _authrepo.Delete(userId);
-      return Ok(response);
+      return Ok(await _authrepo.Delete(userId));
     }
   }
 }
