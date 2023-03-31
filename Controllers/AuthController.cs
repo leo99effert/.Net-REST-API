6d42451c80a9774bc30465c151220abaffd21bf7
leo99effert <leo99effert@outlook.com>
2023-03-31 18:15:23 +0200

    Dto name change

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -11,14 +11,14 @@ namespace Controllers
     }
 
     [HttpPost("Register")]
-    public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+    public async Task<ActionResult<ServiceResponse<int>>> Register(RegisterDto request)
     {
       var response = await _authrepo.Register(new User { Username = request.Username }, request.Password);
       if (!response.Success) return BadRequest(response);
       return Ok(response);
     }
     [HttpPost("Login")]
-    public async Task<ActionResult<ServiceResponse<int>>> Login(UserLoginDto request)
+    public async Task<ActionResult<ServiceResponse<int>>> Login(LoginDto request)
     {
       var response = await _authrepo.Login(request.Username, request.Password);
       if (!response.Success) return BadRequest(response);
--- Dtos/User/UserRegisterDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Dtos.User
-{
-  public class UserRegisterDto
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
