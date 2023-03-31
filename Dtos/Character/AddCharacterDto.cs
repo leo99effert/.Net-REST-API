d2be6e85627de8044f2dca8912f72340c9515c76
leo99effert <leo99effert@outlook.com>
2023-03-31 12:31:11 +0200

    removed hitpoints from addCharacter

--- Dtos/Character/AddCharacterDto.cs
+++ Dtos/Character/AddCharacterDto.cs
@@ -3,7 +3,6 @@ namespace Dtos.Character
   public class AddCharacterDto
     {
         public string Name { get; set; } = "Frodo";
-        public int HitPoints { get; set; } = 100;
         public int Strength { get; set; } = 10;
         public int Defense { get; set; } = 10;
         public int Intelligence { get; set; } = 10;
