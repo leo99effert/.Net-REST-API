37a9a6493c4cbe31827200a84d89c03719c530f5
leo99effert <leo99effert@outlook.com>
2023-04-02 16:11:30 +0200

    charactercontroller cleaning

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -13,33 +13,45 @@ namespace Controllers
     [HttpGet]
     public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
     {
-      return Ok(await _characterService.GetCharacterWithUser());
+      var response = await _characterService.GetCharacterWithUser();
+      if(response.Success) return Ok(response);
+      return BadRequest(response);
     }
     [HttpGet("{id}")]
     public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get(int id)
     {
-      return Ok(await _characterService.GetCharacterWithId(id));
+      var response = await _characterService.GetCharacterWithId(id);
+      if(response.Success) return Ok(response);
+      return BadRequest(response);
     }
     [Authorize]
     [HttpPost]
     public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Post(PostCharacterDto newCharacter)
     {
-      return Ok(await _characterService.CreateCharacter(newCharacter));
+      var response = await _characterService.CreateCharacter(newCharacter);
+      if(response.Success) return Ok(response);
+      return BadRequest(response);
     }
     [HttpDelete("{id}")]
     public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Delete(int id)
     {
-      return Ok(await _characterService.DeleteCharacter(id));
+      var response = await _characterService.DeleteCharacter(id);
+      if(response.Success) return Ok(response);
+      return BadRequest(response);
     }
     [HttpPut]
     public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Put(PutCharacterDto updatedCharacter)
     {
-      return Ok(await _characterService.UpdateCharacter(updatedCharacter));
+      var response = await _characterService.UpdateCharacter(updatedCharacter);
+      if(response.Success) return Ok(response);
+      return BadRequest(response);
     }
     [HttpPost("Skill")]
     public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> PostCharacterSkill(PostCharacterSkillDto newCharacterSkill)
     {
-      return Ok(await _characterService.GiveSkillToCharacter(newCharacterSkill));
+      var response = await _characterService.GiveSkillToCharacter(newCharacterSkill);
+      if(response.Success) return Ok(response);
+      return BadRequest(response);
     }
   }
 }
