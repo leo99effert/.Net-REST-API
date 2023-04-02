c59e4b8434c6602eeb0c067d58360c973801b6ef
leo99effert <leo99effert@outlook.com>
2023-04-02 16:07:19 +0200

    usercontroller cleaning

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -14,25 +14,29 @@ namespace Controllers
     public async Task<ActionResult<ServiceResponse<int>>> Register(RegisterDto request)
     {
       var response = await _userService.Register(new User { Username = request.Username }, request.Password);
-      if (!response.Success) return BadRequest(response);
-      return Ok(response);
+      if (response.Success) return Ok(response);
+      return BadRequest(response);
     }
     [HttpPost("Login")]
     public async Task<ActionResult<ServiceResponse<int>>> Login(LoginDto request)
     {
       var response = await _userService.Login(request.Username, request.Password);
-      if (!response.Success) return BadRequest(response);
-      return Ok(response);
+      if(response.Success) return Ok(response);
+      return BadRequest(response);      
     }
     [HttpGet]
     public async Task<ActionResult<ServiceResponse<List<GetUserDto>>>> Get()
     {
-      return Ok(await _userService.GetUsers());
+      var response = await _userService.GetUsers();
+      if(response.Success) return Ok(response);
+      return BadRequest(response);
     }
     [HttpDelete]
     public async Task<ActionResult<ServiceResponse<int>>> Delete(int userId)
     {
-      return Ok(await _userService.Delete(userId));
+      var response = await _userService.Delete(userId);
+      if(response.Success) return Ok(response);
+      return BadRequest(response);
     }
   }
 }
